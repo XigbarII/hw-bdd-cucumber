87f16dd152c908ca6147ba24ff92a97b0e3823a1
poa2104 Amankwaa <poa2104@columbia.edu>
2021-02-15 04:50:45 +0000

    Working on part 1

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -1,14 +1,40 @@
 # Add a declarative step here for populating the DB with movies.
+#byebug
+Movie.delete_all
+#movies = [{:title => 'Aladdin', :rating => 'G', :release_date => '25-Nov-1992'},
+#     	    {:title => 'The Terminator', :rating => 'R', :release_date => '26-Oct-1984'},
+#    	    {:title => 'When Harry Met Sally', :rating => 'R', :release_date => '21-Jul-1989'},
+#      	  {:title => 'The Help', :rating => 'PG-13', :release_date => '10-Aug-2011'},
+#      	  {:title => 'Chocolat', :rating => 'PG-13', :release_date => '5-Jan-2001'},
+#      	  {:title => 'Amelie', :rating => 'R', :release_date => '25-Apr-2001'},
+#      	  {:title => '2001: A Space Odyssey', :rating => 'G', :release_date => '6-Apr-1968'},
+#      	  {:title => 'The Incredibles', :rating => 'PG', :release_date => '5-Nov-2004'},
+#      	  {:title => 'Raiders of the Lost Ark', :rating => 'PG', :release_date => '12-Jun-1981'},
+#      	  {:title => 'Chicken Run', :rating => 'G', :release_date => '21-Jun-2000'},
+#  	 ]
 
+#movies.each do |movie|
+#  Movie.create!(movie)
+#end
+#byebug
 Given /the following movies exist/ do |movies_table|
+  byebug
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    if Movie.find_by(title: movie[title]).nil?
+      mov = Movie.new
+      mov.title = movie["title"]
+      mov.rating = movie["rating"]
+      mov.release_date = movie["release_date"]
+      mov.save
+    end
   end
   fail "Unimplemented"
 end
 
 Then /(.*) seed movies should exist/ do | n_seeds |
+  byebug
   Movie.count.should be n_seeds.to_i
 end
 
